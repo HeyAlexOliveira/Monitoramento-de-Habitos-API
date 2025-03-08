de003b4bff87fda98787c66aae31dcd93658a713
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-03-08 01:35:46 -0300

    habitos.js

--- Monitoramento de Hábitos  API/monitoramento-habitos-api/routes/habitos.js
+++ Monitoramento de Hábitos  API/monitoramento-habitos-api/routes/habitos.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root", 
-  password: "teamstudy2024",
+  password: "SuaSenhaMySql",
   database: "monitoramento_habitos",
 });
 
